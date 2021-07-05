9d528d4f4f32c2a5b29fd4dd334dbd1d1cf8fdd8
Jorge Viramontes <>
2021-07-05 16:21:04 -0500

    Updated IconCollection to contain icons

--- src/components/iconCollection.js
+++ src/components/iconCollection.js
@@ -1,16 +1,42 @@
 import * as React from "react"
 import * as styles from "./iconCollection.module.css"
+import { SiJavascript, SiHtml5, SiCsswizardry, SiReact, SiGatsby, SiNetlify, SiGithub, SiGoogle } from 'react-icons/si'
+
+import { IconContext } from "react-icons"
 
 const IconCollection = () => (
     <div className={styles.container}>
-        <h2>HTML</h2>
-        <h2>CSS</h2>
-        <h2>JavaScript</h2>
-        <h2>React</h2>
-        <h2>Gatsby</h2>
-        <h2>Netlify</h2>
-        <h2>Github</h2>
-        <h2>Google Domains</h2>  
+        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+            <span><SiHtml5></SiHtml5></span>
+        </IconContext.Provider>
+
+        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+            <span><SiCsswizardry></SiCsswizardry></span>
+        </IconContext.Provider>
+
+        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+            <span><SiJavascript></SiJavascript></span>
+        </IconContext.Provider>
+
+        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+            <span><SiReact></SiReact></span>
+        </IconContext.Provider>
+
+        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+            <span><SiGatsby></SiGatsby></span>
+        </IconContext.Provider>
+
+        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+            <span><SiNetlify></SiNetlify></span>
+        </IconContext.Provider>
+
+        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+            <span><SiGithub></SiGithub></span>
+        </IconContext.Provider>
+
+        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+            <span><SiGoogle></SiGoogle></span>
+        </IconContext.Provider>
     </div>
 )
 
