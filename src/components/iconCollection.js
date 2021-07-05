1ef1ab8950e1132185fd9213e2a5b323983d4839
Jorge Viramontes <>
2021-07-05 16:34:36 -0500

    Added link container class which adds padding and column flexbox.

--- src/components/iconCollection.js
+++ src/components/iconCollection.js
@@ -6,37 +6,62 @@ import { IconContext } from "react-icons"
 
 const IconCollection = () => (
     <div className={styles.container}>
-        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
-            <span><SiHtml5></SiHtml5></span>
-        </IconContext.Provider>
 
-        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
-            <span><SiCsswizardry></SiCsswizardry></span>
-        </IconContext.Provider>
+        <a className={styles.linkContainer}>
+            HTML5
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+                <span><SiHtml5></SiHtml5></span>
+            </IconContext.Provider>
+        </a>
 
-        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
-            <span><SiJavascript></SiJavascript></span>
-        </IconContext.Provider>
+        <a className={styles.linkContainer}>
+            CSS
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+                <span><SiCsswizardry></SiCsswizardry></span>
+            </IconContext.Provider>
+        </a>
 
-        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
-            <span><SiReact></SiReact></span>
-        </IconContext.Provider>
+        <a className={styles.linkContainer}>
+            JavaScript
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+                <span><SiJavascript></SiJavascript></span>
+            </IconContext.Provider>
+        </a>
 
-        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
-            <span><SiGatsby></SiGatsby></span>
-        </IconContext.Provider>
+        <a className={styles.linkContainer}>
+            React
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+                <span><SiReact></SiReact></span>
+            </IconContext.Provider>
+        </a>
 
-        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
-            <span><SiNetlify></SiNetlify></span>
-        </IconContext.Provider>
+        <a className={styles.linkContainer}>
+            Gatsby
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+                <span><SiGatsby></SiGatsby></span>
+            </IconContext.Provider>
+        </a>
 
-        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
-            <span><SiGithub></SiGithub></span>
-        </IconContext.Provider>
+        <a className={styles.linkContainer}>
+            Netlify
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+                <span><SiNetlify></SiNetlify></span>
+            </IconContext.Provider>
+        </a>
 
-        <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
-            <span><SiGoogle></SiGoogle></span>
-        </IconContext.Provider>
+        <a className={styles.linkContainer}>
+            Github
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+                <span><SiGithub></SiGithub></span>
+            </IconContext.Provider>
+        </a>
+
+        <a className={styles.linkContainer}>
+            Google Domains
+            <IconContext.Provider value={{ color: "#1A1A1A", size: "4rem" }}>
+                <span><SiGoogle></SiGoogle></span>
+            </IconContext.Provider>
+        </a>
     </div>
 )
 
